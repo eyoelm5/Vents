7dd8c70d408c727c5074f60d8d0e7780d6b77706
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-11 18:03:22 +0300

    Some changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Social-Blogging-app-front-end",
+  base: "Vents",
   server:{
     proxy:{
       '/api': {
